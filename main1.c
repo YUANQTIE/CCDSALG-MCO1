177ca3ab445574e8817605492a648fe4227aa167
nigelhenryso <nigel_henry_so@dlsu.edu.ph>
2025-07-02 15:16:14 +0800

    Add files via upload

--- main1.c
+++ main1.c
@@ -1,4 +1,4 @@
-#include"grahamScan1.c"
+#include"graham_scan1.c"
 #include<stdio.h>
 int main() {
 	int size = 0;
@@ -22,25 +22,25 @@ int main() {
 		while(fscanf(input, "%lf %lf", &points[i].x, &points[i].y) == 2 && i<nPoints) {
 			i++;
 		}
+		
+		printf("Input the name of the output file: ");
+		scanf("%s", filename);
+	
+		FILE *output = fopen(filename, "w");
+		grahamScan(points, nPoints, &s);
+		for(int i=0; isEmpty(&s) == 0; i++) {
+			point popped = pop(&s);
+			scannedPoints[i].x = popped.x;
+			scannedPoints[i].y = popped.y;
+			printf("Point #%d X: %lf\n", i+1, popped.x);
+			printf("Point #%d Y: %lf\n", i+1, popped.y);
+			size++;
+		}
+		for(int i=size-1; i>=0; i--) {
+			fprintf(output, "%lf %lf\n", scannedPoints[i].x, scannedPoints[i].y);
+		} 
+		fclose(output);
 	}	
 	fclose(input);
-	
-	printf("Input the name of the output file: ");
-	scanf("%s", filename);
-	
-	FILE *output = fopen(filename, "w");
-	grahamScan(points, nPoints, &s);
-	for(int i=0; isEmpty(&s) == 0; i++) {
-		point popped = pop(&s);
-		scannedPoints[i].x = popped.x;
-		scannedPoints[i].y = popped.y;
-		printf("Point #%d X: %lf\n", i+1, popped.x);
-		printf("Point #%d Y: %lf\n", i+1, popped.y);
-		size++;
-	}
-	for(int i=size-1; i>=0; i--) {
-		fprintf(output, "%lf %lf\n", scannedPoints[i].x, scannedPoints[i].y);
-	} 
-	fclose(output);
 }
 
