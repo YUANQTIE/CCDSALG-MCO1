03a3de76743e1ad9abe699734c72d76ff623477b
YUANQTIE <yuanpanlilio@gmail.com>
2025-07-04 14:38:06 +0800

    Update stack.c

--- stack.c
+++ stack.c
@@ -1,23 +1,48 @@
+/***********************************************************************************
+**********************
+Developed by: 
+Yuan Miguel A. Panlilio, 12413135
+Nigel Henry S. So, 12413801
+************************************************************************************
+*********************/
+
 #include <stdio.h>
 #include "stack.h"
+
+ /*Function that creates a new stack.
+    @param *s - the stack created*/
+
 void create(Stack *s) {
 	s->top = -1;
 }
 
+ /*Function that checks if a stack is full.
+    @param *s - a stack.
+    @return 0 if not full and 1 if full*/
+
 int isFull(Stack *s) {
+	int flag = 0;
 	if(s->top == (sizeof(s->points)/16) - 1)
-		return 1;
-	else
-		return 0;
+		flag = 1;
 	
+	return flag;
 }
 
+ /*Function that checks if a stack has not elements.
+    @param *s - a stack.
+    @return 1 if empty, 0 if not.*/
+
 int isEmpty(Stack *s) {
+	int flag = 0;
 	if(s->top == -1)
-		return 1;
-	else
-		return 0;
+		flag = 1;
+
+	return flag;
 }
+
+ /*Function that adds an elements to the top of the stack.
+    @param *s - a stack.
+    @param elem - the point to be added to the stack.*/
 
 void push(Stack *s, point elem) {
 	if(isFull(s) == 0) {
@@ -26,14 +51,22 @@ void push(Stack *s, point elem) {
 	}
 }
 
+ /*Function that removes the top element from the stack.
+    @param *s - a stack.
+    @return the top element of the stack.*/
+
 point pop(Stack *s) {
 	point elem;
-	if(isEmpty(s) == 0) {
+	if(isEmpty(s) == 0) { //checks if an element is not empty first.
 		elem = s->points[s->top];
 		s->top--;
 	}
 	return elem;
 }
+
+ /*Function that shows the the topmost element of the stack.
+    @param *s - a stack.
+    @return the top element of the stack.*/
 
 point top(Stack *s) {
 	point elem;
@@ -42,6 +75,10 @@ point top(Stack *s) {
 	}
 	return elem;
 }
+
+ /*Function that shows the the second topmost element of the stack.
+    @param *s - a stack.
+    @return the element next to the top element of the stack.*/
 
 point nextToTop(Stack *s) {
 	point elem;
